27802ff1a895ef15da7938d592eb6208de595b6e
John L. Singleton <jsinglet@gmail.com>
2015-10-02 12:14:38 -0400

    Updates to checklt version 0.0.2

---
+++ Simple.java
@@ -0,0 +1,48 @@
+import checkers.latticetainting.quals.Level;
+
+
+public class Simple {
+     
+
+    public static void main(String args[]) {
+
+	// This assiangment is from Private -> Public
+	// which is not allowed in our lattice
+        
+	@Level("Public")
+        int e = Simple.testVal();
+
+	// To allow such an assignment, we can "declassify" the value
+	// like so:
+
+	@Level("Private")
+	int secretValue = Simple.testVal();
+
+	@Level("Public")
+	int publicValue = declassify(secretValue);
+	
+	
+
+
+    }
+
+    public static int testVal() {
+	@Level("Private")
+        int notSecret = 1000;
+        return notSecret;
+    }
+
+
+
+    @Level("Public")
+    public static int declassify(@Level("Private") int privateVal){
+        @SuppressWarnings("safe")
+        @Level("Public")
+        int publicVal = privateVal;
+        
+        return publicVal;
+    }  
+
+
+
+}
--- check.clj
+++ check.clj
@@ -1,15 +1,46 @@
 (ns spekl-package-manager.check
- (:require [clojure.java.io :as io]
-            [clojure.tools.logging :as log]))
+(:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
+            [spekl-package-manager.util :as util]
+            [spekl-package-manager.constants :as constants]
+            [spekl-package-manager.download :as download]
+            [spekl-package-manager.package :as package]
+            [clojure.core.reducers :as r]
+            [org.satta.glob :as glob]
+            [clojure.string :as string]
+            [clojure.java.shell :as shell]
+            )
+)
 
-;;
-;; All checks must at least define a "default" check and can optionally define any number of named checks.
-;;
+
+(defn get-classpath-sep []
+  (if (.equals (util/get-my-platform) "windows")
+    ";"
+    ":"))
+
+(defn configure-classpath [extra]
+  (let [cp (*check-configuration* :classpath)]
+    (if (= nil cp)
+      (str  "."  (get-classpath-sep) extra)
+      (string/join (get-classpath-sep) (cons extra (util/expand-glob cp))))))
+
+(defn get-out-dir []
+  (if (not (= nil (*check-configuration* :out)))
+    (list "-d" (*check-configuration* :out))))
+
+(defn get-classpath [extra]
+  (if (= nil (*check-configuration* :out))
+    (list  "-classpath" (str "\"" (configure-classpath extra)  "\""))
+    (list  "-classpath" (str "\"" (configure-classpath extra) (get-classpath-sep) (*check-configuration* :out) "\""))))
+
+
+(defn get-extra-args []
+  (filter (fn [x] (not (= nil x))) (flatten (list (get-out-dir) (get-classpath (resolve-path "lib/checker-lattice-tainting-plugin.jar"))))))
+
 (defcheck default
-  ;; Printing a log message...
-  (log/info  "A message")
 
   ;; running a check
-  (log/info  "Running a check.")  
-  (run "cmd" "arg1" "${installed-package:asset.jar}" "-esc" *project-files-string* ))
+  (log/info  "Running CheckLT.")
+  (run "java" "-jar"  "${lib/checkers.jar}" (get-extra-args) "-processor" "checkers.latticetainting.LatticeTaintingChecker" *project-files*)
+  )
 
